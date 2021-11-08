13a8aa34a4f01719814378cda153bf0002902b16
koraleaxandr <koraleaxandr@gmail.com>
2021-11-08 18:56:31 +0300

    feat 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,41 @@
+const fs = require('fs');
+const path = require('path');
 
+
+const folder = path.join(__dirname, 'files');
+
+// fs.rm(path.join(__dirname, 'files-copy'), {
+//     recursive: false,
+//     force: true
+// }, (err) => {
+//     if (err)
+//         //throw err;
+//         console.log(err);
+// });
+
+
+
+function copyDir() {
+
+    fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+        if (err)
+            //throw err;
+            console.log(err);
+    });
+
+  fs.readdir(folder, {withFileTypes: true}, (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+      fs.copyFile(path.join(__dirname, 'files', file.name),
+        path.join(__dirname, 'files-copy', file.name), 
+        (err) => {
+          if (err) 
+          //throw err;
+          console.log(err);
+        }
+      )}
+    )}
+  );
+}
+
+    copyDir();
