a02b9ee715d01eb8d1a06229edd7e9596c37be43
Alexandr Karatkevich <87863536+koraleaxandr@users.noreply.github.com>
2021-11-09 20:50:11 +0300

    feat add 06 assets + css

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -8,67 +8,44 @@ const stylesDirPath = path.join(__dirname, 'styles');
 // stream.on('data', chunk => data += chunk);
 // stream.on('end', () => console.log('End', data));
 
-fs.rm(path.join(__dirname, 'project-dist'), {recursive: true, force: true}, (err) => {
+fs.rm(path.join(__dirname, 'project-dist'), {recursive: true, force: true}, (err) => { 
   splitCssFiles();
-//   copyDir(path.join(__dirname,'assets'));
+  fs.mkdir(path.join(__dirname, 'project-dist'),{ recursive: true }, (err) => {
+      copyDir(path.join(__dirname,'assets'), path.join(__dirname, 'project-dist', 'assets'));
+  if (err) throw err;
+});     
     if (err)
-       // throw err;
-       console.log(err);
-       fs.mkdir(path.join(__dirname, 'project-dist'), (err) => {
-        if (err)
-            //throw err;
-            console.log(err);
-    });
+        //throw err;
+       console.log(err);       
 });
 
-function copyDir(from,dest) { 
+function copyDir(from, dest) { 
     fs.mkdir(dest,{ recursive: true }, (err) => {
   if (err) throw err;
 });    
     fs.readdir(from, {withFileTypes: true}, (err, files) => {
                 if (err) throw err;
                 files.forEach(file => {
-                    const srcPath = path.join(from,entry.name);
-                    const destPath = path.join(dest,entry.name);
+                    const srcPath = path.join(from, file.name);
+                    const destPath = path.join(dest, file.name);
                             if (file.isFile()) {                                
-                           fs.copyFile(path.join(__dirname, file.name),
-                               path.join(dirPath, file.name),
-                               (err) => {
+                           fs.copyFile(srcPath, destPath, (err) => {
                                    if (err)
                                        //throw err;
                                        console.log(err);
                                })
-                            } else copyDir(srcPath, destPath);
+                            } else {
+                               // fs.mkdir(destPath)
+                            copyDir(srcPath, destPath);
+                            }
                         })
                     });
                 };
 
-
-// async function copyDir(src,dest) {
-//     const entries = await FSP.readdir(src,{withFileTypes:true});
-//     await FSP.mkdir(dest);
-//     for(let entry of entries) {
-//         const srcPath = Path.join(src,entry.name);
-//         const destPath = Path.join(dest,entry.name);
-//         if(entry.isDirectory()) {
-//             await copyDir(srcPath,destPath);
-//         } else {
-//             await FSP.copyFile(srcPath,destPath);
-//         }
-//     }
-// }
-
-function copyFolder() { 
-     fs.readdir(path.join(__dirname,'assets'), {withFileTypes: true}, (err, files) => {
-        copyDir();
-        if (err) throw err; 
-    });
-}
-
-
 function splitCssFiles() {     
     fs.readdir(stylesDirPath, {withFileTypes: true}, (err, files) => {
         if (err) throw err;
+        let data = '';
         files.forEach(file => {
             if (file.isFile() && path.extname(file.name) === '.css') {
                 fs.open(path.join(__dirname, 'styles', file.name), (err, fd) => {
@@ -90,42 +67,6 @@ function splitCssFiles() {
 }
 
 
-// const folder = path.join(__dirname, 'files');
-
-// fs.rm(path.join(__dirname, 'files-copy'), {
-//         recursive: true,
-//         force: true
-//     },
-
-//     (err) => {
-//         copyDir();
-//         if (err)
-//             // throw err;
-//             console.log(err);
-//     });
 
 
 
-// function copyDir() {
-
-//     fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
-//         if (err)
-//             //throw err;
-//             console.log(err);
-//     });
-//     fs.readdir(folder, {
-//         withFileTypes: true
-//     }, (err, files) => {
-//         if (err) throw err;
-//         files.forEach(file => {
-//             fs.copyFile(path.join(__dirname, 'files', file.name),
-//                 path.join(__dirname, 'files-copy', file.name),
-//                 (err) => {
-//                     if (err)
-//                         //throw err;
-//                         console.log(err);
-//                 }
-//             );
-//         });
-//     });
-// }
